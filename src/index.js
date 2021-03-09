d70674bd9c76e710513945f187a58d6a13eff1e1
Artoria01 <79462135+Artoria01@users.noreply.github.com>
2021-03-09 13:59:15 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 const OblivionClient = require('./Structures/OblivionClient');
-const config = require('../config.json');
 
 const client = new OblivionClient(config);
 client.start(process.env.TOKEN);
